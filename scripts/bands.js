e13177608d33919d82d936b872ba4324df9b4059
Michelle Primiani <primianimichelle@gmail.com>
2023-08-02 11:14:05 -0400

    Created addEventListener for bands.js & venues.js

--- scripts/bands.js
+++ scripts/bands.js
@@ -1,15 +1,44 @@
-import { getBands } from "./database.js";
+import { getBands, getBookings, getVenues } from "./database.js";
 
 const bands = getBands();
+const venues = getVenues();
+const bookings = getBookings();
 
 export const CurrentBands = () => {
   let html = "";
   html = "<ul>";
 
   for (const band of bands) {
-    html += `<li>${band.name}</li>`;
+    html += `<li data-type="bands" 
+    data-id="${band.id}" 
+    data-name="${band.name}" 
+    data-members="${band.numOfMembers}"
+    data-genre="${band.genre}"
+    data-year="${band.yearFormed}"
+    >${band.name}</li>`;
   }
 
   html += "</ul>";
   return html;
 };
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+
+  let venuesPlayed = [];
+  const bandId = itemClicked.dataset.id;
+
+  if (itemClicked.dataset.type === "bands") {
+    for (const booking of bookings) {
+      if (parseInt(bandId) === booking.bandId)
+        for (const venue of venues) {
+          if (booking.venueId === venue.id) {
+            venuesPlayed.push(venue.name);
+          }
+        }
+    }
+    window.alert(`${itemClicked.dataset.name} is playing at ${venuesPlayed} `);
+  }
+});
+
+// displays all of the venue at which the band is playing.
--- scripts/venues.js
+++ scripts/venues.js
@@ -9,9 +9,30 @@ export const CurrentVenues = () => {
   html = "<ul>";
 
   for (const venue of venues) {
-    html += `<li>${venue.name}</li>`;
+    html += `<li data-type="venue" data-name="${venue.name}" data-id="${venue.id}">${venue.name}</li>`;
   }
 
   html += "</ul>";
   return html;
 };
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  let bookedBands = [];
+  const venueName = itemClicked.dataset.id;
+
+  if (itemClicked.dataset.type === "venue") {
+    for (const booking of bookings) {
+      if (booking.venueId === parseInt(venueName)) {
+        for (const band of bands) {
+          if (band.id === booking.bandId) {
+            bookedBands.push(band.name);
+          }
+        }
+      }
+    }
+    window.alert(`This venue has booked ${bookedBands}`);
+  }
+});
+
+// displays all of the bands that have booked the venue.
